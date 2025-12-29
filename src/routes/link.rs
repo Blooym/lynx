e1973a0e2846e40eec7857023f09114b72c58900
Lyna <git@blooym.dev>
2025-12-29 15:29:13 +0100

    feat: return text for status codes too

--- src/routes/link.rs
+++ src/routes/link.rs
@@ -14,33 +14,37 @@ use tracing::{debug, error};
 pub async fn get_link_redirect_handler(
     State(state): State<AppState>,
     Path(link_id): Path<LinkId>,
-) -> Result<Redirect, StatusCode> {
+) -> Result<Redirect, (StatusCode, &'static str)> {
     let config = state.config.read().await;
     let Some(link) = config.links().get(&link_id) else {
         debug!("Link ID '{}' not found in configuration", link_id);
-        return Err(StatusCode::NOT_FOUND);
+        return Err((
+            StatusCode::NOT_FOUND,
+            "This link does not exist or is no longer available.",
+        ));
     };
-
     if !link_valid(&link_id, link).await.map_err(|err| {
         error!("Failed to validate Link ID '{}': {}", link_id, err);
-        StatusCode::INTERNAL_SERVER_ERROR
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Something went wrong, please try again later.",
+        )
     })? {
-        return Err(StatusCode::NOT_FOUND);
+        return Err((
+            StatusCode::NOT_FOUND,
+            "This link does not exist or is no longer available.",
+        ));
     }
-
     debug!("Redirecting Link ID '{}' -> '{}'", link_id, link.redirect,);
-
     Ok(Redirect::temporary(link.redirect.as_str()))
 }
 
 async fn link_valid(link_id: &str, link_data: &Link) -> Result<bool> {
-    // Ensure the link is still enabled.
     if link_data.disabled {
         debug!("Link ID '{}' is disabled", link_id);
         return Ok(false);
     }
 
-    // Ensure the link has not expired.
     if let Some(invalid_after) = link_data.invalid_after
         && invalid_after < SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs()
     {
